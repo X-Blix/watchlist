c44883361c1c450f1a9459a9b4086011c8f0b2c5
X-Blix <xblix@qq.com>
2024-04-26 22:32:24 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask import request, url_for, redirect, flash
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import UserMixin, login_user, current_user
+from flask_login import LoginManager
+
+from flask_login import login_required, logout_user
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -23,16 +29,17 @@ app.config['SECRET_KEY'] = 'dev'  #设置签名所需的密钥
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
+login_manager = LoginManager(app)  # 实例化扩展类
 
 
-@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
-def initdb(drop):
-    """Initialize the database."""
-    if drop:  # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+login_manager.login_view = 'login'
+
+
 
 @app.cli.command()
 def forge():
@@ -41,6 +48,7 @@ def forge():
 
     # 全局的两个变量移动到这个函数内
     name = 'X-Blix'
+
     books = [
     {'title': '红楼梦', 'author': '曹雪芹'},
     {'title': '活着', 'author': '余华'},
@@ -64,14 +72,59 @@ def forge():
     click.echo('Done.')
 
 
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)  # 设置密码
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)  # 设置密码
+        db.session.add(user)
+
+    db.session.commit()  # 提交数据库会话
+    click.echo('Done.')
+
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True,autoincrement = True)
+    name = db.Column(db.String(20))
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))  # 密码散列值
+
+    def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数
+        self.password_hash = generate_password_hash(password)  # 将生成的密码保持到对应字段
+
+    def validate_password(self, password):  # 用于验证密码的方法，接受密码作为参数
+        return check_password_hash(self.password_hash, password)  # 返回布尔值
+
 
 class Book(db.Model):  # 表名将会是 book
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 标题
     author = db.Column(db.String(10))  # 作者
+
+
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
 
 
 @app.context_processor
@@ -86,15 +139,17 @@ def page_not_found(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # 判断是否是 POST 请求
-        # 获取表单数据
-        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
-        author = request.form.get('author')
-        # 验证数据
+
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
+
+        title = request.form['title']
+        author = request.form['author']
 
         if not title or not author:
             flash('Invalid input.')  # 显示错误提示
             return redirect(url_for('index'))  # 重定向回主页
-        
+
         # 保存表单数据到数据库
         book = Book(title=title, author=author)  # 创建记录
         db.session.add(book)  # 添加到数据库会话
@@ -105,8 +160,9 @@ def index():
     books = Book.query.all()
     return render_template('index.html', books=books) # 重定向回主页
 
-# 编辑图书条目 
+# 编辑图书条目
 @app.route('/book/edit/<int:book_id>', methods=['GET', 'POST'])
+@login_required
 def edit(book_id):
     book = Book.query.get_or_404(book_id)
 
@@ -117,7 +173,7 @@ def edit(book_id):
         if not title or not author:
             flash('Invalid input.')
             return redirect(url_for('edit', book_id=book_id))  # 重定向回对应的编辑页面
-    
+
         book.title = title  # 更新标题
         book.author = author  # 更新年份
         db.session.commit()  # 提交数据库会话
@@ -126,11 +182,64 @@ def edit(book_id):
 
     return render_template('edit.html', book=book)  # 传入被编辑的电影记录
 
-# 编辑删除条目 
+# 编辑删除条目
 @app.route('/book/delete/<int:book_id>', methods=['POST'])  # 限定只接受 POST 请求
+@login_required  # 登录保护
 def delete(book_id):
     book = Book.query.get_or_404(book_id)  # 获取电影记录
     db.session.delete(book)  # 删除对应的记录
     db.session.commit()  # 提交数据库会话
     flash('Item deleted.')
     return redirect(url_for('index'))  # 重定向回主页
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success.')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required  # 用于视图保护，后面会详细介绍
+def logout():
+    logout_user()  # 登出用户
+    flash('Goodbye.')
+    return redirect(url_for('index'))  # 重定向回首页
+
+
+# 支持设置用户名字
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        # current_user 会返回当前登录用户的数据库记录对象
+        # 等同于下面的用法
+        # user = User.query.first()
+        # user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
